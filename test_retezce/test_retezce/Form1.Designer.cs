37e09ced28e6740da7c283bbff11aa5a86b158d4
syrovym05 <97514843+syrovym05@users.noreply.github.com>
2022-04-25 09:39:11 +0200

    Add files via upload

---
+++ test_retezce/test_retezce/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace test_retezce
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.Location = new System.Drawing.Point(73, 95);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(209, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(395, 95);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(178, 238);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackColor = System.Drawing.Color.DarkOrange;
+            this.button1.FlatAppearance.BorderSize = 2;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.NavajoWhite;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(73, 277);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 56);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Potvrdit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(69, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Zadej testovaci vetu:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(391, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(182, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Nevyskytující se písmena";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(69, 189);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(183, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Nejčastější písmeno : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Sienna;
+            this.ClientSize = new System.Drawing.Size(700, 402);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ test_retezce/test_retezce/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test_retezce
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //string veta1 = "_a_ nam zabili  Ferdinanda.";
+        //string veta2 = "Prazdn_ retezec  ma hodnotu \"\" nebo _tring.Empty";
+        //string veta3 = "Inst_nce  String je nemenn_.";
+        //string veta4 = "Halda j_ prakticky __om_z__a pamet.";
+
+        string abeceda = "abcdefghijklmnopqrstuvwxyz";
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {           
+            string veta = textBox1.Text;
+            veta = veta.ToLower();
+            while (veta.Contains("  "))
+            {
+                veta = veta.Replace("  ", " ");
+            }
+            textBox1.Text = veta;
+            
+            string[] slova = veta.Split(' ', '_');
+
+           
+            
+
+            for (int i = 0; i < abeceda.Length; i++)
+            {
+                if (!(veta.Contains(abeceda[i])))
+                {
+                    listBox1.Items.Add(abeceda[i]);
+                }
+                
+
+            }
+
+
+
+            string veta2 = veta;
+            
+
+            char[] pole_pismen = veta2.ToCharArray();
+            Array.Sort(pole_pismen);            
+            int pocet_char = 0;
+            int max_char = 0;
+            char max_c = 'a';
+            char znak = 'a';
+                                   
+            foreach (char c in pole_pismen)
+            {
+                //MessageBox.Show(((char)(znak)).ToString());                
+                if (abeceda.Contains(c))
+                {                                        
+                    if (c != znak)
+                    {                        
+                        znak++;
+                        pocet_char = 0;
+                    }
+                    if (c == znak)
+                    {
+                        pocet_char++;
+                    }
+                    if (max_char < pocet_char)
+                    {
+                        max_char = pocet_char;
+                        max_c = c;
+                        pocet_char = 0;
+                    }
+                    //MessageBox.Show(max_char.ToString());
+                    //MessageBox.Show(znak.ToString());
+
+                }
+            }
+            label4.Text += " " + max_c.ToString();
+
+            
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
